6eb5f3d71be74780d971e47564dab411804cde50
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-11-03 10:25:54 +0100

    Day 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,12 @@
 import java.awt.*;
-import java.io.*;
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
 
+        // Deklaracja zmiennych
         String s = "";
         int x;
-
-//        for (int i = 0; i < 11; i++) {
-//            s+= "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
-//            for (int j = 0; j < 11; j++) {
-//
-//                if(i == 0 && j == 0){
-//                    System.out.print("|   |");
-//                    s += "|\t\t|";
-//                }
-//                else if(i == 0 && j <= 9){
-//                    System.out.print( + j + "  |");
-//                    s +="\t" + j + "\t|";
-//                }
-//                else if(i == 0 && j == 10){
-//                    System.out.print( + j + " |");
-//                    s +="\t" + j + "\t|";
-//                }
-//                else if(j == 0 && i <= 9){
-//                    System.out.print("|" + i + "  |");
-//                    s += "|\t" + i + "\t|";
-//                }
-//                else if(j == 0 && i == 10){
-//                    System.out.print("|" + i + " |");
-//                    s += "|\t" + i + "\t|";
-//                }
-//                else if(i > 0 && j > 0){
-//                    x = i * j;
-//                    if(x <= 9){
-//                        System.out.print(x + "  |");
-//                        s +="\t" + x + "\t|";
-//                    }
-//                    else if (x >= 10 && x < 100) {
-//                        System.out.print(x + " |");
-//                        s +="\t" + x + "\t|";
-//                    }
-//                    else if (x ==100) {
-//                        System.out.print(x + "|");
-//                        s +="\t" + x + "\t|";
-//                    }
-//                }
-//            }
-//            System.out.print("\n");
-//            s += "\n";
-//        }
 
         // Część konsolowa
         for (int i = 0; i < 11; i++) {
@@ -89,7 +45,7 @@ public class Main {
 
         // Część JOptionPane
         for (int i = 0; i < 11; i++) {
-            s+= "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
+            s+= "--------------------------------------------------------------------------------------------------------------------------\n";
             for (int j = 0; j < 11; j++) {
                 if(i == 0 && j == 0){
                     s += "|\t\t|";
@@ -107,15 +63,17 @@ public class Main {
             }
             s += "\n";
         }
-        s+= "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
+        s+= "--------------------------------------------------------------------------------------------------------------------------\n";
 
+        // Stworzenie pola tekstowego
         JTextArea tx = new JTextArea(s);
-        tx.setEditable(false);
+        tx.setEditable(false); //Ustawienei możliwości edycji
         tx.setOpaque(false);
-        tx.setTabSize(3);
-        tx.setPreferredSize(new Dimension(750, 400));
+        tx.setTabSize(2); // Ustawienie wielkości tabulacji
+        tx.setPreferredSize(new Dimension(530, 400)); //Ustawienie rozmiaru pola tekstowego
 
-       JOptionPane.showMessageDialog(null, tx , "Tabliczka", JOptionPane.PLAIN_MESSAGE);
+        // Przekazanie pola tekstowego do massageboxa i wyświetlenie go na ekranie
+       JOptionPane.showMessageDialog(null, tx , "Tabliczka", JOptionPane.INFORMATION_MESSAGE);
         System.exit(0);
     }
 }
